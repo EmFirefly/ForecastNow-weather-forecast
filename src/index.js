7095b0f9f4976f6c047f6f7a12d2342a51290feb
EmFirefly <meganvzyl@gmail.com>
2025-06-29 20:37:35 +0200

    added api

--- src/index.js
+++ src/index.js
@@ -1,11 +1,29 @@
+function getWeatherData(response) {
+    console.log(response.data);
+    let temperatureElement = document.querySelector('.current-temperature');
+    let weatcherIconElement = document.querySelector('.weather-icon');
+    let humidityElement = document.querySelector('#humidity');
+    let windSpeedElement = document.querySelector('#windspeed');
+    let cityElement = document.querySelector('#city-main');
+    
+    cityElement.innerHTML = response.data.city ;
+    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+    weatcherIconElement.innerHTML = response.data.condition.icon;
+    humidityElement.innerHTML = response.data.temperature.humidity;
+    windSpeedElement.innerHTML = Math.round(response.data.wind.speed);
+
+}   
+function fetchWeatherData(city) {
+    let apiKey = "5f0e384aoacc74261536bb0t2474bbf6";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(getWeatherData);
+}
+
 function searchFormSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector('#search-input');
-    let cityElement = document.querySelector('#city-main');
-    cityElement.innerHTML = searchInput.value;
+    
+    fetchWeatherData(searchInput.value);
 }
-
-
-
 let searchFormElement = document.querySelector('#search-form');
-searchFormElement.addEventListener('submit', searchFormSubmit);
+searchFormElement.addEventListener('submit', searchFormSubmit); 
